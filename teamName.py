9af37c3b2817448f4902523bf3257faad82d19e0
Dow Zhong <dowzhong@gmail.com>
2023-07-17 21:24:06 +1000

    Initial commit

---
+++ teamName.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+import numpy as np
+
+nInst=50
+currentPos = np.zeros(nInst)
+def getMyPosition (prcSoFar):
+    global currentPos
+    (nins,nt) = prcSoFar.shape
+    if (nt < 2):
+        return np.zeros(nins)
+    lastRet = np.log(prcSoFar[:,-1] / prcSoFar[:,-2])
+    rpos = np.array([int(x) for x in 2000 * lastRet / prcSoFar[:,-1]])
+    currentPos = np.array(map(int, currentPos+rpos))
+    return currentPos
+
+    
